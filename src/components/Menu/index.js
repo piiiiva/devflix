2266bd287c142c1d0260b0000130a98654ba22fe
piiiiva <piiiiva@gmail.com>
2020-07-28 13:48:46 -0300

    Using react-router-dom; Creating a Main component; Fixing Footer

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/devflix.svg'
 import './Menu.css'
 import Button from '../Button/index'
@@ -6,11 +7,11 @@ import Button from '../Button/index'
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="DevFlix Logo"/>
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import App from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio masterblaster -> Criar uma página 404 bonita
+const Pagina404 = () => (<div>Erro 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
