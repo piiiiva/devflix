176ed3f2b6069c9e1ea6220d9f97d9f7855eacee
piiiiva <piiiiva@gmail.com>
2020-07-31 01:38:50 -0300

    URL_TOP  value from Heroku in production and localhost in dev

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -33,7 +33,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categories';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categories'
+      : 'https://pivaflix.herokuapp.com/categories';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
