164b0b22c5d7a39cbe9085d4fd67fa37697e02b5
piiiiva <piiiiva@gmail.com>
2020-08-02 00:19:20 -0300

    Update URL_BACK_END_TOP in dev enviroment

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080'
+  ? 'http://localhost:8080/categories'
   : 'https://pivaflix.herokuapp.com';
 
 export default {
